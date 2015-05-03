d73dd56060285f853583aae2e3e5c639ddf55105
Julia Farrell <farre230@umn.edu>
2015-05-02 20:18:05 -0500

    Edit 2

--- js/script.js
+++ js/script.js
@@ -14,19 +14,19 @@ $(document).scroll(function(){
 	}
 });
 
-var winHeight = $(window).innerHeight(); // setting winHeight to be the variable for browser height
+// var winHeight = $(window).innerHeight(); // setting winHeight to be the variable for browser height
 	
-$(document).ready(function() {
+// $(document).ready(function() {
 
 
-    $(".panel").height(winHeight);
-    $("body").height(winHeight*$(".panel").length*0.5); // sets height of body based on number of divs on the page
+//     $(".panel").height(winHeight);
+//     $("body").height(winHeight*$(".panel").length*0.5); // sets height of body based on number of divs on the page
 
-});
+// });
 
-	window.addEventListener('resize', function (event) {
-    	$(".panel").height($(window).innerHeight()); // resizes each panel for responsiveness
-	});
+	// window.addEventListener('resize', function (event) {
+ //    	$(".panel").height($(window).innerHeight()); // resizes each panel for responsiveness
+	// });
 
 	$(window).on('scroll',function(){
     	$(".panel_left").css('bottom',$(window).scrollTop()*-1); // enables reverse scrolling of left side of the screen
@@ -42,6 +42,6 @@ $(document).ready(function() {
 //         var totalHeight = $('.panel_left').height(); // measures the height of entire page
 //         var posToScroll = Math.round(_cur_top / individualDivHeight) * individualDivHeight; // calculates how much scrolling needs to be done to snap to next or previous panel
 
-//         $('html, body').stop().animate({scrollTop: posToScroll}, 150); // automatically scrolls to next or previous panel based on the results of the previous line of code
+//         $('html, body').stop().animate({scrollTop: posToScroll}, 100); // automatically scrolls to next or previous panel based on the results of the previous line of code
 //     });
 // });
