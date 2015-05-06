902afbe3dfe44a305a985e24f60c90d91eb030b7
Julia Farrell <farre230@umn.edu>
2015-05-06 01:59:49 -0500

    Edit 7
    Final edits before class!

--- js/script.js
+++ js/script.js
@@ -12,6 +12,214 @@ $(document).scroll(function(){
 		$(".navbar").css("position", "absolute");
 		$(".navbar").css("top", "770px");
 	}
+
+	if (y<1500) {
+		$(".nav1").css("color", "#e1e1e1");
+	}
+	if (y>1500) {
+		$(".nav1").css("color", "#041547");
+	}
+	if (y>2310)  {
+		$(".nav1").css("color", "#e1e1e1");
+	}
+	if (y<2310) {
+		$(".nav2").css("color", "#e1e1e1");
+	}
+	if (y>2310) {
+		$(".nav2").css("color", "#041547");
+	}
+	if (y>3080)  {
+		$(".nav2").css("color", "#e1e1e1");
+	}
+	if (y<3080) {
+		$(".nav3").css("color", "#e1e1e1");
+	}
+	if (y>3080) {
+		$(".nav3").css("color", "#041547");
+	}
+	if (y>3850)  {
+		$(".nav3").css("color", "#e1e1e1");
+	}
+	if (y<3850) {
+		$(".nav4").css("color", "#e1e1e1");
+	}
+	if (y>3850) {
+		$(".nav4").css("color", "#041547");
+	}
+	if (y>4620)  {
+		$(".nav4").css("color", "#e1e1e1");
+	}
+	if (y<4620) {
+		$(".nav5").css("color", "#e1e1e1");
+	}
+	if (y>4620) {
+		$(".nav5").css("color", "#041547");
+	}
+	if (y>5390)  {
+		$(".nav5").css("color", "#e1e1e1");
+	}
+	if (y<5390) {
+		$(".nav6").css("color", "#e1e1e1");
+	}
+	if (y>5390) {
+		$(".nav6").css("color", "#041547");
+	}
+	if (y>6160)  {
+		$(".nav6").css("color", "#e1e1e1");
+	}
+	if (y<6160) {
+		$(".nav7").css("color", "#e1e1e1");
+	}
+	if (y>6160) {
+		$(".nav7").css("color", "#041547");
+	}
+	if (y>6930)  {
+		$(".nav7").css("color", "#e1e1e1");
+	}
+	if (y<6930) {
+		$(".nav8").css("color", "#e1e1e1");
+	}
+	if (y>6930) {
+		$(".nav8").css("color", "#041547");
+	}
+	if (y>7700)  {
+		$(".nav8").css("color", "#e1e1e1");
+	}
+	if (y<7700) {
+		$(".nav9").css("color", "#e1e1e1");
+	}
+	if (y>7700) {
+		$(".nav9").css("color", "#041547");
+	}
+	if (y>8470)  {
+		$(".nav9").css("color", "#e1e1e1");
+	}
+	if (y<8470) {
+		$(".nav10").css("color", "#e1e1e1");
+	}
+	if (y>8470) {
+		$(".nav10").css("color", "#041547");
+	}
+	if (y>9240)  {
+		$(".nav10").css("color", "#e1e1e1");
+	}
+	if (y<9240) {
+		$(".nav11").css("color", "#e1e1e1");
+	}
+	if (y>9240) {
+		$(".nav11").css("color", "#041547");
+	}
+	if (y>10010)  {
+		$(".nav11").css("color", "#e1e1e1");
+	}
+	if (y<10010) {
+		$(".nav12").css("color", "#e1e1e1");
+	}
+	if (y>10010) {
+		$(".nav12").css("color", "#041547");
+	}
+	if (y>10780)  {
+		$(".nav12").css("color", "#e1e1e1");
+	}
+	if (y<10780) {
+		$(".nav13").css("color", "#e1e1e1");
+	}
+	if (y>10780) {
+		$(".nav13").css("color", "#041547");
+	}
+	if (y>11550)  {
+		$(".nav13").css("color", "#e1e1e1");
+	}
+	if (y<11550) {
+		$(".nav14").css("color", "#e1e1e1");
+	}
+	if (y>11550) {
+		$(".nav14").css("color", "#041547");
+	}
+	if (y>12320)  {
+		$(".nav14").css("color", "#e1e1e1");
+	}
+	if (y<12320) {
+		$(".nav15").css("color", "#e1e1e1");
+	}
+	if (y>12320) {
+		$(".nav15").css("color", "#041547");
+	}
+	if (y>13090)  {
+		$(".nav15").css("color", "#e1e1e1");
+	}
+	if (y<13090) {
+		$(".nav16").css("color", "#e1e1e1");
+	}
+	if (y>13090) {
+		$(".nav16").css("color", "#041547");
+	}
+	if (y>13860)  {
+		$(".nav16").css("color", "#e1e1e1");
+	}
+	if (y<13860) {
+		$(".nav17").css("color", "#e1e1e1");
+	}
+	if (y>13860) {
+		$(".nav17").css("color", "#041547");
+	}
+	if (y>14630)  {
+		$(".nav17").css("color", "#e1e1e1");
+	}
+	if (y<14630) {
+		$(".nav18").css("color", "#e1e1e1");
+	}
+	if (y>14630) {
+		$(".nav18").css("color", "#041547");
+	}
+	if (y>15400)  {
+		$(".nav18").css("color", "#e1e1e1");
+	}
+	if (y<15400) {
+		$(".nav19").css("color", "#e1e1e1");
+	}
+	if (y>15400) {
+		$(".nav19").css("color", "#041547");
+	}
+	if (y>16170)  {
+		$(".nav19").css("color", "#e1e1e1");
+	}
+	if (y<16170) {
+		$(".nav20").css("color", "#e1e1e1");
+	}
+	if (y>16170) {
+		$(".nav20").css("color", "#041547");
+	}
+	if (y>16940)  {
+		$(".nav20").css("color", "#e1e1e1");
+	}
+	if (y<16940) {
+		$(".nav21").css("color", "#e1e1e1");
+	}
+	if (y>16940) {
+		$(".nav21").css("color", "#041547");
+	}
+	if (y>17710)  {
+		$(".nav21").css("color", "#e1e1e1");
+	}
+	if (y<17710) {
+		$(".nav22").css("color", "#e1e1e1");
+	}
+	if (y>17710) {
+		$(".nav22").css("color", "#041547");
+	}
+	if (y>18480)  {
+		$(".nav22").css("color", "#e1e1e1");
+	}
+	if (y<18480) {
+		$(".nav23").css("color", "#e1e1e1");
+	}
+	if (y>18480) {
+		$(".nav23").css("color", "#041547");
+	}
+	if (y>19250)  {
+		$(".nav23").css("color", "#e1e1e1");
+	}
 });
 
 // var winHeight = $(window).innerHeight(); // setting winHeight to be the variable for browser height
